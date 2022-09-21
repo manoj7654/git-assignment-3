05690ae20836ef879313cfa14060823894fd00ac
Manoj Kumar <manojsfstm5@gmail.com>
2022-09-21 22:01:41 +0530

    code fix by manoj

--- prime.js
+++ prime.js
@@ -1,12 +1,18 @@
- let num=19;
-let count=0;
+function Prime(num){
+let fact=0;
 for(let i=1;i<=num;i++){
-  if(num%i==0){
-  count++;
+   if(num%i==0){
+     fact++;
+   }
+ }
+if(fact==2){
+  return true;
   }
+  return false;
 }
-  if(count==1){
-  console.log("Prime");
-  }else{
-  console.log("Not Prime);
+let res=Prime(19);
+if(res==true){
+   console.log("Prime");
+}else{
+   console.log("Not Prime");
 }
